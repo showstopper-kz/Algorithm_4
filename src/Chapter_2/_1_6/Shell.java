a1036c448072ac1b141a760ee6640d35c76b4d4a
zhuyongqi <563078214@qq.com>
2020-11-05 21:21:22 +0800

    11.5 21:21

--- src/Chapter_2/_1_6/Shell.java
+++ src/Chapter_2/_1_6/Shell.java
@@ -10,15 +10,16 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Shell  {
     public static void sort(Comparable[] a){
-        int N=a.length;
-        int h=1;
-        while(h<N) h=3*h+1;
-        while(h>=1) {
-            for (int i = h; i < N; i++) {
-                for(int j=i;j>h&&less(a[j],a[j-h]);j-=h)
-                    exch(a,j,j-h);
-            }
-        }
+       int N=a.length;
+       int h=1;
+       while (h < N/3) h = 3*h + 1;
+       while (h >= 1){
+           for(int i=h;i<N;i++){
+               for(int j=i;j>=h&&less(a[j],a[j-h]);j -= h)
+                   exch(a,j,j-h);
+           }
+       }
+       h /= 3;
     }
 
     private static boolean less(Comparable v,Comparable w)
---
+++ src/Chapter_2/_3_1/Quick.java
@@ -0,0 +1,16 @@
+package Chapter_2._3_1;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick {
+    public static void sort(Comparable[] a){
+        StdRandom.shuffle(a);
+       //wq sort(a,0,);
+    }
+
+    private static void sort(Comparable[] a,int lo,int hi){
+        if(hi <= lo) return;
+
+    }
+
+}
