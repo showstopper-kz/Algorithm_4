ebd99369c3f74c73ab6a2928b2c7ddfe0edcf6d1
zhuyongqi <abnormalled@outlook.com>
2020-11-18 13:31:02 +0800

    graph_dfs

---
+++ src/Chapter_4/_1_2/DepthFirstSearch.java
@@ -0,0 +1,27 @@
+package Chapter_4._1_2;
+
+public class DepthFirstSearch {
+
+    private boolean[] marked;
+    private int count;
+
+    public DepthFirstSearch(Graph g,int s){
+        marked = new boolean[g.V()];
+    }
+
+    private void dfs(Graph g,int v){
+        marked[v] = true;
+        count++;
+        for (int w : g.adj(v)) {
+            if( !marked[w] ) dfs(g,w);
+        }
+    }
+
+    public boolean marked(int w){
+        return marked[w];
+    }
+
+    public int count(){
+        return count;
+    }
+}
